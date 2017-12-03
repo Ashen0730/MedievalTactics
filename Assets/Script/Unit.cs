42c3b4c04db6258a777b4cbecde8c211717603f0
heissdevin <33069205+heissdevin@users.noreply.github.com>
2017-12-03 03:08:20 -0500

    hasAttacked AND no moving after attacking

--- Assets/Script/Unit.cs
+++ Assets/Script/Unit.cs
@@ -19,6 +19,7 @@ public class Unit : MonoBehaviour {
 	public bool isFocused;
 	public bool hasBetrayed;
 	public bool isMoved = false;
+	public bool hasAttacked=false;
 	//public bool tileSelected = false;
 
 	[SerializeField]
@@ -29,10 +30,15 @@ public class Unit : MonoBehaviour {
 		if (map.FocusedUnit == null) {
 			print ("selecting unit");
 			map.FocusedUnit = this;
+
+			if(!map.FocusedUnit.hasAttacked){
 			//if the focused unit has not been moved then...
-			if (!map.FocusedUnit.isMoved) {
-				map.highlightMovable (map.FocusedUnit.move + map.GetTile (x, y).cost, map.FocusedUnit.x, map.FocusedUnit.y);
-				map.highlightAttackable (map.FocusedUnit.range, map.FocusedUnit.x, map.FocusedUnit.y);
+				if (!map.FocusedUnit.isMoved) {
+					map.highlightMovable (map.FocusedUnit.move + map.GetTile (x, y).cost, map.FocusedUnit.x, map.FocusedUnit.y);
+				//if the focused unit has not attacked then...
+				}if (!map.FocusedUnit.hasAttacked) {
+						map.highlightAttackable (map.FocusedUnit.range, map.FocusedUnit.x, map.FocusedUnit.y);
+				}
 			} else { //if the unit has been moved, deselects it
 				print("deselecting");
 				map.FocusedUnit = null;
@@ -44,7 +50,7 @@ public class Unit : MonoBehaviour {
 			map.clearHighlightAttack ();
 		} else {
 
-			if (map.GetUnit (map.FocusedUnit.x, map.FocusedUnit.y).isEnemy != this.isEnemy && map.GetTile(this.x, this.y).IsAttackable) {
+			if (map.GetUnit (map.FocusedUnit.x, map.FocusedUnit.y).isEnemy != this.isEnemy && map.GetTile(this.x, this.y).IsAttackable&&!map.FocusedUnit.hasAttacked) {
 				print (map.FocusedUnit.name + " attacking to " + this.name);
 				print (this.health + "current");
 				map.AttackTo (map.GetUnit (map.FocusedUnit.x, map.FocusedUnit.y), this);
@@ -53,14 +59,106 @@ public class Unit : MonoBehaviour {
 				map.clearHighlightMove ();
 				map.FocusedUnit = null;
 			} else {
-				print ("cant attack your team");
+				print ("cant attack");
 			}
 		}
+
+		/*
+		if (map.FocusedUnit == null) {
+			print("selecting unit");
+			map.FocusedUnit = this;
+			//highlight moveable tile;
+			map.highlightMovable(this.move, this.x, this.y);
+		} else {
+			if (this == map.FocusedUnit) {
+				print ("Deselecting from here");
+				map.clearHighlightMove ();
+				map.FocusedUnit = null;
+			}
+			else if (!map.unitisAttacking) {
+
+				print("not attacking unit");
+				print (this.x + " " + this.y);
+				moveUnit (map.destinationTile, this);
+				isMoved = true;
+				map.unitisAttacking = true;
+				map.highlightAttackable (map.FocusedUnit.range, map.FocusedUnit.x, map.FocusedUnit.y);
+			} else {
+				if (map.FocusedUnit == this) {
+					print("deselect");
+					map.FocusedUnit = null;
+					map.clearHighlightAttack();
+					map.unitisAttacking = false;
+				} else {
+					print (this.health);
+					//print (map.FocusedUnit.gameObject.name+ " attacking to " + this.gameObject.name);
+					map.AttackTo (map.FocusedUnit, this);
+					print (this.health);
+					this.isDead ();
+					map.FocusedUnit = null;
+					map.unitisAttacking = false;
+					map.clearHighlightAttack();
+				}
+			}
+		}
+
+		/*
+		if (map.GetTile(x, y).IsAttackable)
+		{
+			map.AttackTo(map.FocusedUnit, this);
+			return;
+		}
+
+		if (null != map.FocusedUnit && this != map.FocusedUnit)
+		{
+			map.FocusedUnit.isFocused = false;
+			map.clearHighlightAttack();
+			map.clearHighlightMove ();
+		}
+
+		isFocused = !isFocused;
+		if (isFocused)
+		{
+			map.highlightMovable(move, x, y);
+			map.highlightAttackable(range, x, y);
+		}
+		else
+		{
+			map.clearHighlightAttack();
+			map.clearHighlightMove ();
+
+		}
+		*/
+
+		/*	
+		if (!map.unitisAttacking) {
+			map.FocusedUnit = this;
+			this.isMoved = true;
+			map.unitisAttacking = true;
+			//print (map.FocusedUnit.gameObject.name);
+			//print("x: " + map.FocusedUnit.x + " y: " + map.FocusedUnit.y);
+			//print(map.GetTile(map.FocusedUnit.x,  map.FocusedUnit.y).X + " " +map.GetTile(map.FocusedUnit.x,  map.FocusedUnit.y).Y);
+			map.highlightAttackable(map.FocusedUnit.range, map.FocusedUnit.x, map.FocusedUnit.y);
+		} else {
+			if (map.FocusedUnit == this) {
+				//print ("cant attack yourself");
+				return;
+			}else{
+				print (this.health);
+				//print (map.FocusedUnit.gameObject.name+ " attacking to " + this.gameObject.name);
+				map.AttackTo (map.FocusedUnit, this);
+				//this.loseHealth (this.ATK);
+				print (this.health);
+				this.isDead ();
+				map.FocusedUnit = null;
+				map.unitisAttacking = false;
+				map.eraseHighlight();
+			}
+		}*/
 	}
 		
 
 	public void moveUnit(Tile destination, Unit focused){
-		print ("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
 		if((destination.X==(focused.x+1)||destination.X==(focused.x-1))&&(destination.Y==(focused.y+1)||destination.Y==(focused.y-1))){
 			focused.x = destination.X;
 			focused.y = destination.Y;
@@ -97,9 +195,5 @@ public class Unit : MonoBehaviour {
 			isMoved = true;
 			movesCount = 0;
 		}
-	}
-
-	public int getMovesCount(){
-		return movesCount;
 	}
 }
